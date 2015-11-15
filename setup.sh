3c6f9dd0af96cd02d34e2a9bbd77349acd417b2e
zhangxj <zhangxj@zhangxj-T62Series.(none)>
2015-11-15 10:20:13 +0800

    ..

---
+++ setup.sh
@@ -0,0 +1,12 @@
+wget https://bootstrap.pypa.io/ez_setup.py
+python ez_setup.py
+easy_install tornado==4.0
+easy_install redis
+easy_install pymongo
+easy_install pymongo
+easy_install msgpack-python
+wget http://jaist.dl.sourceforge.net/project/pydes/pydes/2.0.1/pyDes-2.0.1.zip
+unzip pyDes-2.0.1.zip
+cd pyDes-2.0.1
+python setup.py install
+
